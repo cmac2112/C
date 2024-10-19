00e0529da006861583e81c6a6ac671364006e842
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-10-19 13:28:16 -0500

    assembly test x86

---
+++ other/size.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int main(){
+    printf("size of int: %d bytes\n", sizeof(int));
+    printf("size of char: %d bytes\n", sizeof(char));
+    return 0;
+    /* 1 bytes = 8 bits*/
+}
+/* gcc*/
